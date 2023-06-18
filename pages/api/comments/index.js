a959d8092fa2ccb510c3fc8fc82731d0361ab283
Ahmed-Magdy-El-Sayed <109208478+Ahmed-Magdy-El-Sayed@users.noreply.github.com>
2023-06-18 16:21:34 +0300

    Update index.js

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,9 +1,15 @@
 import {comments} from "../../../data/comments";
+import { fileURLToPath } from 'url';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default function handler(req, res) {
     if(req.method === "POST"){
         comments.push(JSON.parse(req.body));
+        fs.writeFile(
+            path.join(__dirname,"..","..","..","data","comments.js"),
+            "export let comments = "+ JSON.stringify(comments)
+        );
         res.status(200).json()
     }
     else if(req.method === "GET"){
@@ -12,12 +18,20 @@ export default function handler(req, res) {
     else if(req.method === "DELETE"){
         let index = comments.findIndex(c => c.id === JSON.parse(req.body))
         comments.splice(index,1);
+        fs.writeFile(
+            path.join(__dirname,"..","..","..","data","comments.js"),
+            "export let comments = "+ JSON.stringify(comments)
+        );
         res.status(200).json()
     }
     else if(req.method === "PATCH"){
         let id = JSON.parse(req.body).id;
         let index = comments.findIndex(c => c.id === id)
         comments.splice(index,1,JSON.parse(req.body));
+        fs.writeFile(
+            path.join(__dirname,"..","..","..","data","comments.js"),
+            "export let comments = "+ JSON.stringify(comments)
+        );
         res.status(200).json()
     }
 }
